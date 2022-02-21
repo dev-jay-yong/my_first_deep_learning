ace21100b2bc27ba20156fbd9d88c68828c6ceff
dev.jayyong <jayyong.dev@gmail.com>
2022-02-21 18:52:38 +0900

    1부터 10까지 keras 모델로 예측하기

---
+++ study/01_predict_one_to_ten.py
@@ -0,0 +1,30 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+
+x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+y = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+# 딥러닝의 기본식 => y = ax + b
+# h(x) = wx + b (w는 weight 가중치를 의미, h는 hypothesis 가설을 의미
+
+model = Sequential()
+model.add(Dense(1, input_dim=1, activation='relu'))
+model.compile(
+    loss='mean_squared_error',  # 손실 함수는 평균제곱법 사용
+    optimizer='adam',  # 최적화 함수는 adam 옵티마이저 사용
+    metrics=['accuracy']  # 방식은 정확도를 통해 판정
+)
+
+model.fit(x, y, epochs=500, batch_size=1)
+
+loss, acc = model.evaluate(x, y, batch_size=1)
+
+print(f"loss : {loss}")
+print(f"accuracy: {acc}")
+
+# loss : 38.5
+# accuracy: 0.0
+
+loss : 0.006178527604788542
+accuracy: 0.10000000149011612
