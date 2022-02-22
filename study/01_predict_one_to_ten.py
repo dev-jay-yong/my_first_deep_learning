05676aa88b0a83ee07229fa8630e8589dee3e3af
dev.jayyong <jayyong.dev@gmail.com>
2022-02-22 18:31:33 +0900

    ai 예측이 항상 동일하게 예측값을 반환하도록 수정

--- study/01_predict_one_to_ten.py
+++ study/01_predict_one_to_ten.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense
 
@@ -7,6 +8,8 @@ y = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
 # 딥러닝의 기본식 => y = ax + b
 # h(x) = wx + b (w는 weight 가중치를 의미, h는 hypothesis 가설을 의미
+
+tf.random.set_seed(5) # 시드를 고정하여야 항상 동일한 결과를 받을 수 있음
 
 model = Sequential()
 model.add(Dense(1, input_dim=1, activation='relu'))
@@ -19,6 +22,7 @@ model.compile(
 model.fit(x, y, epochs=500, batch_size=1)
 
 loss, acc = model.evaluate(x, y, batch_size=1)
+print(model.predict([1, 2, 3, 4, 5, 10, 11]))
 
 print(f"loss : {loss}")
 print(f"accuracy: {acc}")
